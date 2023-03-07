16e2093c74b2f456305926d06855ccf8bd69d44c
Nuradiyana <me@nooradiana.com>
2023-03-07 17:04:52 +0700

    Fixing null $body

--- src/Client.php
+++ src/Client.php
@@ -86,9 +86,11 @@ final class Client implements ClientInterface
             /** @var array<array-key, string> $body */
             $body = \json_decode((string) $response->getBody(), true);
 
-            Assert::keyExists($body, 'access_token');
+            if(\is_array($body)) {
+                Assert::keyExists($body, 'access_token');
 
-            $this->accessToken = $body['access_token'];
+                $this->accessToken = $body['access_token'];
+            }
         }
     }
 
